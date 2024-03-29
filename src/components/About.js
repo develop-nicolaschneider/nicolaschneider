f35117e278089d8a48c65a0c0b30cb9f225eb6c5
Nicola Schneider <nicola.schneider@students.bfh.ch>
2024-03-29 12:53:41 +0100

    Updated About component

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,45 @@ import { useRef } from "react";
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
 import '../assets/styles/About.css'
+import {useTranslation} from "react-i18next";
+
+const aboutSlides = [
+    {
+        slideId: "slider-1",
+        slideTitle: "Motorrad",
+        slideText: "",
+    },
+    {
+        slideId: "slider-2",
+        slideTitle: "About me",
+        slideText: "",
+    },
+    {
+        slideId: "slider-3",
+        slideTitle: "Travel",
+        slideText: "",
+    },
+    {
+        slideId: "slider-4",
+        slideTitle: "Photography",
+        slideText: "",
+    },
+    {
+        slideId: "slider-5",
+        slideTitle: "Hockey",
+        slideText: "",
+    },
+    {
+        slideId: "slider-6",
+        slideTitle: "EHC Koppigen",
+        slideText: "",
+    }
+]
 
 const About = () => {
+    const { t} = useTranslation()
     const my_age = new Date(2000, 10, 5)
     const age = Math.abs(new Date(Date.now() - my_age).getUTCFullYear() - 1970)
-
     const sliderRef = useRef(null)
 
     const onPrevBtnClicked = () => {
@@ -22,70 +56,20 @@ const About = () => {
 
     return (
         <div className="about-div">
-            <h1>About</h1>
-            <h2>{age} years old</h2>
+            <h1>{t("aboutTitle")}</h1>
+            <h2>{t("aboutSubtitle")} {age} {t("aboutSubtitle2")}</h2>
             <div className="slides-container">
                 <div className="slider" ref={sliderRef}>
-                    <div
-                        id="slider-1"
-                        className="slides">
-                        <div className="slide-content">
-                            <h2>Motorrad</h2>
-                            <p>
-
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        id="slider-2"
-                        className="slides">
-                        <div className="slide-content">
-                            <h2>About me</h2>
-                            <p>
-
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        id="slider-3"
-                        className="slides">
-                        <div className="slide-content">
-                            <h2>Travel</h2>
-                            <p>
-
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        id="slider-4"
-                        className="slides">
-                        <div className="slide-content">
-                            <h2>Photography</h2>
-                            <p>
-
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        id="slider-5"
-                        className="slides">
-                        <div className="slide-content">
-                            <h2>Hockey</h2>
-                            <p>
-
-                            </p>
-                        </div>
-                    </div>
-                    <div
-                        id="slider-6"
-                        className="slides">
-                        <div className="slide-content">
-                            <h2>EHC Burgdorf</h2>
-                            <p>
-
-                            </p>
-                        </div>
-                    </div>
+                    {aboutSlides.map((slide, key) => {
+                        return (
+                            <div id={slide.slideId} className="slides" key={key}>
+                                <div className="slide-content">
+                                    <h2>{slide.slideTitle}</h2>
+                                    <p>{slide.slideText}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
                 <div className="slider-buttons-div">
                     <button onClick={() => onPrevBtnClicked()} type="button" id="prevSlide" className="slide-button">
